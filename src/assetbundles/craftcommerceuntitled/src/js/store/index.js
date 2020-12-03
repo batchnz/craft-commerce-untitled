83fc8f79bd69ae92e79506445edae73b54ed3271
Josh Smith <josh@batch.nz>
2020-12-04 09:29:46 +1300

    docs(store): add docs to the vuex store

--- src/assetbundles/craftcommerceuntitled/src/js/store/index.js
+++ src/assetbundles/craftcommerceuntitled/src/js/store/index.js
@@ -7,12 +7,21 @@ import * as SETTINGS from "../constants/settings-types";
 
 Vue.use(Vuex);
 
+/**
+ * Defines the default variant configuration settings
+ * Used with mutations to ensure all properties exist in settings objects
+ * @type {Object}
+ */
 const defaultSettings = {
   [SETTINGS.METHOD]: null,
   [SETTINGS.FIELD]: null,
   [SETTINGS.VALUES]: {},
 };
 
+/**
+ * Vuex Store
+ * @type {Object}
+ */
 export default new Vuex.Store({
   state: {
     ui: {
@@ -32,36 +41,124 @@ export default new Vuex.Store({
     variantConfigurationTypeFields: [],
   },
   mutations: {
+    /**
+     * Sets the variant configurations data
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATIONS](state, payload) {
       state.variantConfigurations = payload;
     },
+
+    /**
+     * sets the variant configuration type fields data
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_TYPE_FIELDS](state, payload) {
       state.variantConfigurationTypeFields = payload;
     },
+
+    /**
+     * Sets the UI loading state
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_IS_LOADING](state, payload) {
       state.ui.isLoading = payload;
     },
+
+    /**
+     * Sets a variant configuration object
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION](state, payload) {
       state.variantConfiguration = payload;
     },
+
+    /**
+     * Sets variant configurations
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.UPDATE_VARIANT_CONFIGURATION](state, payload) {
       state.variantConfigurations = {
         ...state.variantConfigurations,
         ...payload,
       };
     },
+
+    /**
+     * Sets the variant configuration title
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_TITLE](state, payload) {
       state.variantConfiguration.title = payload;
     },
+
+    /**
+     * Sets variant configuration fields
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_FIELDS](state, payload) {
       state.variantConfiguration.fields = payload;
     },
+
+    /**
+     * Sets configuration values
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_VALUES](state, payload) {
       state.variantConfiguration.values = payload;
     },
+
+    /**
+     * Sets the entire variant settings object
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param object payload
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_SETTINGS](state, payload) {
       state.variantConfiguration.settings = payload;
     },
+
+    /**
+     * Sets variant configuration settings by type
+     * Current types are `price` and `stock`
+     * Settings data is merged with defaults to ensure all properties are set
+     *
+     * Current defaults are:
+     *   - `method`: either `all`, `field` or `skip`
+     *   - `field`: The field name when method is set to `field`
+     *   - `values`: An object of values for the selected field. A key of `value` is used for non relation values
+     *
+     * @author Josh Smith <josh@batch.nz>
+     * @since  1.0.0
+     * @param object state
+     * @param string type The settings type (`price` or `stock`)
+     * @param object data The data to store
+     */
     [MUTATIONS.SET_VARIANT_CONFIGURATION_SETTINGS_TYPE](state, { type, data }) {
       if (!state.variantConfiguration.settings[type]) {
         state.variantConfiguration.settings[type] = {};
@@ -146,6 +243,12 @@ export default new Vuex.Store({
     },
   },
   getters: {
+    /**
+     * Returns whether all variant configuration fields have been selected
+     * @author Josh Smith <josh@batch.nz>
+     * @param  object  state
+     * @return Boolean
+     */
     isAllFieldsSelected(state) {
       const handles = state.variantConfigurationTypeFields.map(
         (field) => field.handle
@@ -156,6 +259,14 @@ export default new Vuex.Store({
 
       return filteredArray.length === handles.length;
     },
+
+    /**
+     * Returns whether all variant configuration values for the passed field handle have been selected
+     * @author Josh Smith <josh@batch.nz>
+     * @param  object  state
+     * @param  object  getters
+     * @return Boolean
+     */
     isAllValuesSelected(state, getters) {
       return (handle) => {
         const values = getters.fieldValuesByHandle[handle];
@@ -167,11 +278,26 @@ export default new Vuex.Store({
         return filteredArray.length === values.length;
       };
     },
+
+    /**
+     * Returns the variant configuration field types that have been selected
+     * @author Josh Smith <josh@batch.nz>
+     * @param  object state
+     * @return array
+     */
     selectedFields(state) {
       return state.variantConfigurationTypeFields.filter((field) =>
         state.variantConfiguration.fields.includes(field.handle)
       );
     },
+
+    /**
+     * Returns the selected variant configuration field values, keyed by field handle
+     * @author Josh Smith <josh@batch.nz>
+     * @param  object state
+     * @param  object getters
+     * @return object
+     */
     selectedFieldValuesByHandle(state, getters) {
       const values = {};
 
@@ -187,6 +313,13 @@ export default new Vuex.Store({
 
       return values;
     },
+
+    /**
+     * Returns all variant configuration field type values, keyed by field handle
+     * @author Josh Smith <josh@batch.nz>
+     * @param  object state
+     * @return object
+     */
     fieldValuesByHandle(state) {
       const values = {};
       state.variantConfigurationTypeFields.forEach((field) => {
